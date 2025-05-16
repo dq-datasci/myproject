fa5aade88c1d464ad1f650f9b42938886c3d03a7
dq-datasci <david.a.quintela@gmail.com>
2025-05-16 10:47:12 -0400

    Agregamos la carpeta templates y dinamica y index

--- home.py
+++ home.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+@app.route("/dinamica")
+def hello_world_dinamica():
+    return render_template("dinamica.html")
+
+
